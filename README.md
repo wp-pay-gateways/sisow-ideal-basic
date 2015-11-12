5580bf92ab45d5a4fb30cf26a2bbb5fbd6826b63
Remco Tolsma <info@remcotolsma.nl>
2015-11-12 14:06:32 +0100

    Mark as deprecated.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# WordPress Pay Gateway: Sisow - iDEAL Basic
+# [DEPRECATED] WordPress Pay Gateway: Sisow - iDEAL Basic
 
 **Sisow - iDEAL Basic driver for the WordPress payment processing library.**
+
+## Production Environment
+
+**Payment Server URL:** https://www.sisow.nl/Sisow/iDeal/IssuerHandler.ashx  
+
+## Test Environment
+
+**Payment Server URL:** https://www.sisow.nl/Sisow/iDeal/IssuerHandler.ashx/test  
--- src/Integration.php
+++ src/Integration.php
@@ -1,8 +1,13 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_GatewayIntegration {
+class Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_Integration {
 	public function __construct() {
-		$this->id = 'sisow-ideal-basic';
+		$this->id            = 'sisow-ideal-basic';
+		$this->name          = 'Sisow - iDEAL Basic';
+		$this->url           = 'https://www.sisow.nl/';
+		$this->dashboard_url = 'https://www.sisow.nl/Sisow/iDeal/Login.aspx';
+		$this->provider      = 'sisow';
+		$this->deprecated    = true;
 	}
 
 	public function get_config_factory_class() {
