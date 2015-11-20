580ea41d0f30055b034899b4c9c4043aee0b4e2f
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 13:42:00 +0100

    Added support an custom config factory.

---
+++ src/ConfigTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: Sisow - iDEAL Basic config test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_ConfigTest extends Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_Config {
+	public function get_payment_server_url() {
+		return 'https://www.sisow.nl/Sisow/iDeal/IssuerHandler.ashx/test';
+	}
+}
--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Title: Sisow - iDEAL Basic integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
 		$this->id            = 'sisow-ideal-basic';
@@ -8,5 +16,9 @@ class Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_Integration extends Pronamic_WP_
 		$this->dashboard_url = 'https://www.sisow.nl/Sisow/iDeal/Login.aspx';
 		$this->provider      = 'sisow';
 		$this->deprecated    = true;
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_ConfigFactory';
 	}
 }
